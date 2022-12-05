5b3256497d113b27ea25d5d34d0c8edebf54859d
Filipe Francisco Franknberger <111152966+filipe-f-franknberger@users.noreply.github.com>
2022-12-05 20:39:02 -0300

    Add files via upload

--- Estacionamento/src/com/classes/DAO/MonitoramentoDAO.java
+++ Estacionamento/src/com/classes/DAO/MonitoramentoDAO.java
@@ -1,5 +1,55 @@
 package com.classes.DAO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import com.classes.DTO.Monitoramento;
+import com.classes.Conexao.Conexao;
 
 public class MonitoramentoDAO {
 
+    final String NOMEDATABELA = "monitoramento";
+    
+    public boolean inserir(Monitoramento Monitoramento) {
+        try {
+            Connection conn = Conexao.conectar();
+            String sql = "INSERT INTO " + NOMEDATABELA + " (descricao) VALUES (?);";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.executeUpdate();
+            ps.close();
+            conn.close();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public boolean alterar(Monitoramento Monitoramento) {
+        try {
+            Connection conn = Conexao.conectar();
+            String sql = "UPDATE " + NOMEDATABELA + " SET descricao = ? WHERE codigo = ?;";
+            PreparedStatement ps = conn.prepareStatement(sql);
+    
+            ps.executeUpdate();
+            ps.close();
+            conn.close();
+            return true;
+        } catch (Exception e) {
+        	 e.printStackTrace();
+             return false;
+        }
+    }
+    public boolean excluir(Monitoramento Monitoramento) {
+        try {
+            Connection conn = Conexao.conectar();
+            String sql = "DELETE FROM " + NOMEDATABELA + " WHERE codigo = ?;";
+            PreparedStatement ps = conn.prepareStatement(sql);
+      
+            ps.executeUpdate();
+            ps.close();
+            conn.close();
+            return true;
+        } catch (Exception e) {
+        	 e.printStackTrace();
+             return false;
+        }
+    }
 }
--- Estacionamento/src/com/classes/DTO/Monitoramento.java
+++ Estacionamento/src/com/classes/DTO/Monitoramento.java
@@ -68,6 +68,7 @@ public class Monitoramento {
 		
 		
 		
+		
 	}
 	
 	public void retirarVeiculo() {
